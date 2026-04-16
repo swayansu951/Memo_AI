cb8047d9bc06c1c0e6cad640839dde4ebdb5425c
swayansu951 <swayansusahu951@gmail.com>
2026-04-16 11:28:26 +0530

    corrected the stt model config

--- agent/stt.py
+++ agent/stt.py
@@ -2,7 +2,7 @@ from faster_whisper import WhisperModel
 
 class STTEngine:
     def __init__(self, model_size = "./whisper_model"):
-        self.model = WhisperModel(model_size, device= "cuda",compute_type= "int8")
+        self.model = WhisperModel(model_size, device= "cpu",compute_type= "int8")
     
     def transcribe(self, audio_file):
         segment, _ = self.model.transcribe(audio_file, beam_size=1)
