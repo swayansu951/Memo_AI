ea487a4d389c77e3f7a6a3cfc85008f4bd45a189
swayansu951 <swayansusahu951@gmail.com>
2026-04-16 20:09:16 +0530

    bug fixed :)

--- agent/stt.py
+++ agent/stt.py
@@ -1,8 +1,13 @@
 from faster_whisper import WhisperModel
+import os 
+
 
 class STTEngine:
     def __init__(self, model_size = "./whisper_model"):
         self.model = WhisperModel(model_size, device= "cpu",compute_type= "int8")
+        hf_token = os.getenv("HF_TOKEN")  
+        if not hf_token:
+            print("HF_TOKEN not found in .env")
     
     def transcribe(self, audio_file):
         segment, _ = self.model.transcribe(audio_file, beam_size=1)
